5437dd33ff852caf8b4db8935aa21340d55ff702
S SIDHARTH <sidharthpunalur@gmail.com>
2025-06-04 01:05:15 +0530

    CORS added

--- xyphx/settings.py
+++ xyphx/settings.py
@@ -20,13 +20,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'drf_yasg',
     'visitors',
     'maintenance',
     'apply',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
